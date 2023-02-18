1d9408781a64f76f2133e6abd61c3f6b7ea60848
Natanael dos Santos Feitosa <52074821+natanfeitosa@users.noreply.github.com>
2023-02-18 15:38:19 +0000

    fix dynamic fragment removal

--- dist/soursop.cjs.js
+++ dist/soursop.cjs.js
@@ -214,6 +214,9 @@ function reconcileChildren(wipFiber, elements) {
       let deletes = function(fiber) {
         fiber.effectTag = "DELETION";
         if (fiber.child) {
+          if (fiber.parent.type == Fragment && fiber.sibling) {
+            deletes(fiber.sibling);
+          }
           deletes(fiber.child);
         }
       };
